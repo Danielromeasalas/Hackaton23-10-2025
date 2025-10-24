7e825287a0cec42e036f5c1233aa76156b366ac4
Sofi1108 <sofiatorcalvalentin@gmail.com>
2025-10-24 07:48:48 +0200

    [ADD] format

--- Arrays/ejercicio20.java
+++ Arrays/ejercicio20.java
@@ -9,21 +9,20 @@ public class ejercicio20 {
             arrayInicial[i] = new Random().nextInt(5) + 1;
             System.out.print(arrayInicial[i] + " ");
         }
-            //Ordenar
-            for(int i=0; i < arrayInicial.length-1; i++){
-                for(int j=0; j < (arrayInicial.length-1-i); j++){
-                    if(arrayInicial[j] > arrayInicial[j+1]){
-                        int aux=arrayInicial[j];
-                        arrayInicial[j]=arrayInicial[j+1];
-                        arrayInicial[j+1]=aux;
-                    }
+        //Ordenar
+        for(int i=0; i < arrayInicial.length-1; i++){
+            for(int j=0; j < (arrayInicial.length-1-i); j++){
+                if(arrayInicial[j] > arrayInicial[j+1]){
+                    int aux=arrayInicial[j];
+                    arrayInicial[j]=arrayInicial[j+1];
+                    arrayInicial[j+1]=aux;
                 }
             }
-            System.out.print("\nPuntuaciones ordenadas: ");
-            for(int i=0;i<arrayInicial.length;i++){
-                System.out.print(arrayInicial[i]+" ");
-            }
-
+        }
+        System.out.print("\nPuntuaciones ordenadas: ");
+        for(int i=0;i<arrayInicial.length;i++){
+            System.out.print(arrayInicial[i]+" ");
+        }
     }
 }
 
